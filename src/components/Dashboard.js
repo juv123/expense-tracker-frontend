74495f19ed15a6afbd3172c1ba3a1819a2a52531
juv123 <deegaaug@gmail.com>
2025-01-29 17:14:12 +0530

    expense update delete

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 const Dashboard=()=>{
-    return(
+    const [bgcolor,setBgcolor]=useState("green-600");
+        return(
         <>
-           <div class="w-64 h-screen bg-green-300 text-white p-6 m-3 mt-3">
-    <h2 class="text-2xl font-bold mb-6 text-white">Dashboard</h2>
+           <div className="w-64 h-screen bg-green-300 text-white p-6 m-3 mt-3">
+    <h2 className="text-2xl font-bold mb-6 text-white">Dashboard</h2>
     <ul>
-        <li><Link to="/Expenses" class="block py-3 px-4 mb-2 rounded-lg hover:bg-green-600 transition duration-200 ease-in-out">Expenses</Link></li>
-        <li><Link to="/Analytics" class="block py-3 px-4 mb-2 rounded-lg hover:bg-green-600 transition duration-200 ease-in-out">Analytics</Link></li>
+        <li><Link to="/Expenses" className="block py-3 px-4 mb-2 rounded-lg hover:bg-green-600 transition duration-200 ease-in-out">Expenses</Link></li>
+        <li><Link to="/Analytics" className="block py-3 px-4 mb-2 rounded-lg hover:bg-green-600 transition duration-200 ease-in-out">Analytics</Link></li>
     </ul>
 </div>
 </>
